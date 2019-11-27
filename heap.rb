108cb4577ffb1db14160419e92d80ad20b5816a5
Cheng Yi Lee <chengyil@sg.ibm.com>
2019-11-28 01:56:02 +0800

    heap

--- heap.rb
+++ heap.rb
@@ -27,7 +27,11 @@ class Heap
     end
 
     def parent
-      @parent ||= Node.new((index-1) / 2, @heap)
+      if (index - 1) / 2 >= 0
+        @parent ||= Node.new((index-1) / 2, @heap)
+      else
+        nil
+      end
     end
   end
 
@@ -37,8 +41,12 @@ class Heap
     @data = values
   end
 
-  def root_node
+  def root
     Node.new(0,self)
+  end
+
+  def last_node
+    Node.new(size-1,self)
   end
 
   def [](index)
@@ -52,8 +60,51 @@ class Heap
   def count
     @data.count
   end
+
+  def swap(node, node2)
+    @data[node.index], @data[node2.index] = @data[node2.index], @data[node.index]
+  end
+
+  def add(val)
+    @data << val
+    heapify_up
+  end
+
+  def heapify_up
+    node = last_node
+    while node.parent && node.val < node.parent.val
+      swap(node, node.parent)
+      node = node.parent
+    end
+  end
+
+  def heapify_down
+    node = root
+    p node.val
+    while(node.left)
+      smaller_node = node.left
+      if node.right && node.right.val < smaller_node.val
+        smaller_node = node.right
+      end
+      if node.val < smaller_node.val
+        break
+      else
+        swap(node,smaller_node)
+        node = smaller_node
+      end
+    end
+  end
+
+  def poll
+    item = root.val
+    swap(root, last_node)
+    @data.pop
+    heapify_down
+    item
+  end
 end
 
 heap = Heap.new([1,2,3,4,5,6,7,8,9,10])
-root = heap.root_node
-p root.left.left.left.parent.right.val
+root = heap.root
+heap.add(0)
+p heap.poll
